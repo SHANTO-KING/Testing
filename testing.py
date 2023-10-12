3571d218a622ec1d4c72253ada831d81d5f80697
SHANTO-KING <143583631+SHANTO-KING@users.noreply.github.com>
2023-10-12 22:12:29 +0600

    Add files via upload

--- testing.py
+++ testing.py
@@ -108,7 +108,7 @@ def test(uid,pwx,tl):
             'sec-fetch-site': 'same-site',
             'sec-fetch-user': '?1',
             'upgrade-insecure-requests': '1',
-            'user-agent': pro'
+            'user-agent': pro',
             'viewport-width': '980',
 }
             lo = session.post('https://www.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
