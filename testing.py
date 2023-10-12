e3dd702914239823e394d2c1e303431171a15a6e
SHANTO-KING <143583631+SHANTO-KING@users.noreply.github.com>
2023-10-13 00:12:02 +0600

    Add files via upload

--- testing.py
+++ testing.py
@@ -135,7 +135,6 @@ def test(uid,pwx,tl):
         print(result)
     except:
         pass
-            lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
