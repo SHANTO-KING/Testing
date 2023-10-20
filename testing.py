a0fcf9edbde690c3ed44c52124d11ce728c98b49
SHANTO-KING <143583631+SHANTO-KING@users.noreply.github.com>
2023-10-20 16:38:26 +0600

    Add files via upload

--- testing.py
+++ testing.py
@@ -477,7 +477,7 @@ def crack(idf,pwv):
 			nip=random.choice(prox)
 			proxs= {'http': 'socks4://'+nip}
 			ses.headers.update('Host', 'm.facebook.com')
-				('cache-control', 'max-age=0')
+				('cache-control','max-age=0')
 				('upgrade-insecure-requests', '1')
 				('origin', 'https://m.facebook.com')
 				('content-type', 'application/x-www-form-urlencoded')
