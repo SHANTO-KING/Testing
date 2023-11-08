f8e3d36715e9aed6b13903534a4b371fa30f005d
SHANTO-KING <143583631+SHANTO-KING@users.noreply.github.com>
2023-11-08 12:29:06 +0600

    Add files via upload

--- testing.py
+++ testing.py
@@ -117,7 +117,7 @@ def chk():
   id = "|".join(uuid)
   print("\n\n\x1b[37;1m  YOUR ID : \033[94m"+id) 
   try: 
-    httpCaht = requests.get("https://github.com/cyberattack07/approval.txt/blob/main/approval.txt").text 
+    httpCaht = requests.get("https://github.com/SHANTO-KING/Approval/blob/main/Approval.txt").text 
     if id in httpCaht: 
       print("\033[92m  YOUR ID IS ACTIVE. .......\033[97m") 
       msg = str(os.geteuid()) 
